3b7172d7d5d1142a21481669fce6650a9fb9371b
saeedalzahrani <49519757+saeedalzahrani@users.noreply.github.com>
2019-07-15 10:43:16 +0300

    Update app-people.js

--- bin/app-people.js
+++ bin/app-people.js
@@ -7,111 +7,99 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
 })
 const db = mongoose.connection
 
-const Place = require('../models/place.js')
+const Person = require('../models/person.js')
+
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
 }
 
-// CRUD Actions
-const create = function (name, latitude, longitude, country) {
+const index = function () {
   /* Add Code Here */
-  const placeParams = {
-    name,
-    latitude,
-    longitude,
-    country
-  }
-  Place.create(placeParams)
-   .then(
-     place => console.log(place),
-    error => console.error
-   )
+  Person.find()
+  .then((people) => {
+    people.forEach(person => console.log(person.toJSON()))
+  })
+  .catch(console.error)
   .then(done)
 }
 
-const index = function () {
- /* Add Code Here */
- Place.find()
- .then(
-   places => places.forEach(place => console.log(place)),
-   error => console.error
- )
- .then(done)
+const show = function (id) {
+  /* Add Code Here */
+  Person.findById(id)
+  .then((person) => console.log(person.toJSON()))
+  .catch(console.error)
+  .then(done)
 }
 
-const show = function (id) {
- /* Add Code Here */
- Place.findById(id)
- .then(
-   place => console.log(place.toJSON()),
-   error => console.error
- )
- .then(done)
+const destroy = function (id) {
+  /* Add Code Here */
+  Person.findById(id)
+  .then((person) => person.remove())
+  .catch(console.error)
+  .then(done)
 }
 
 const update = function (id, field, value) {
- /* Add Code Here */
-Place.findById(id)
-.then(
-  place => {
-    place[field] = value;
-    return place.save()
-  },
-  error => console.error
-)
-.then(done)
+  /* Add Code Here */
 }
 
-
-const destroy = function (id) {
- /* Add Code Here */
- Place.findById(id)
- .then(
-   place => place.remove(),
-   error => console.error
- )
- .then(done)
+const create = function (firstName, lastName, dob, height, weight) {
+  /* Add Code Here */
+  const personParams = {
+    name: {
+      firstName: firstName,
+      lastName: lastName
+    },
+    dob: dob,
+    height: height,
+    weight: weight
+  }
+  Person.create(personParams)
+    .then((person) => console.log(person.toJSON()))
+    .catch(console.error)
+    .then(done)
 }
 
-// UI
 db.once('open', function () {
- const command = process.argv[2]
+  const command = process.argv[2]
 
- let field
- let id
+  let field
+  let id
 
- switch (command) {
-   case 'create':
-     const name = process.argv[3]
-     const latitude = process.argv[4]
-     const longitude = process.argv[5]
-     const country = process.argv[6]
+  switch (command) {
+    case 'create':
+      const firstName = process.argv[3]
+      const lastName = process.argv[4]
+      const dob = process.argv[5]
+      const height = process.argv[6]
+      const weight = process.argv[7]
 
-     create(name, latitude, longitude, country)
+      create(firstName, lastName, dob, height, weight)
 
-     break
+      break
 
-   case 'show':
-     id = process.argv[3]
-     show(id)
-     break
+    case 'show':
+      id = process.argv[3]
+      show(id)
+      break
 
-   case 'update':
-     id = process.argv[3]
-     field = process.argv[4]
-     const value = process.argv[5]
-     update(id, field, value)
-     break
+    case 'update':
+      id = process.argv[3]
+      field = process.argv[4]
+      const value = process.argv[5]
+      update(id, field, value)
+      break
 
-   case 'destroy':
-     id = process.argv[3]
-     destroy(id)
-     break
+    case 'destroy':
+      id = process.argv[3]
+      destroy(id)
+      break
 
     default:
       index()
-    break
+
+      break
   }
 })
 
-module.exports = place;
+module.exports = Person
